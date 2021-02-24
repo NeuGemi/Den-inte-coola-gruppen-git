45b287a49f98bbd5b4589429fcf14738f1c442f1
Lidok123 <79506296+Lidok123@users.noreply.github.com>
2021-02-24 17:02:36 +0100

    Healthbar
    Jag har börjat på en enkel healthbar i en ny scen

---
+++ Assets/Codes/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public Healthpoints healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Gör att man börjar med 100% hälsa -Henry
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gör att man tar skada när man klickar på pilen nedåt -Henry
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            TakeDamage(20); 
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+}
